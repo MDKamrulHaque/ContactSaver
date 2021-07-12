bf1f078dfd9eaf4f54f0bf56d5b02b4d851f53cb
MD Kamrul Haque <55250085+MDKamrulHaque@users.noreply.github.com>
2021-07-12 15:17:51 +0100

    Add project files.

---
+++ ContactSaver/Phone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactSaver
+{
+    public partial class Phone : Form
+    {
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+
+            comboBox1.SelectedIndex = -1;
+
+            textBox1.Focus();
+
+        }
+    }
+}
